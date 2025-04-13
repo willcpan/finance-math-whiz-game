57c176e56d6e7da9f4d967fe506c75a2307d40e0
Will Pan <willcpan@gmail.com>
2025-04-13 18:10:30 -0400

    Fix: Configure react-router basename for GH Pages

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,8 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      {/* Add basename prop here */}
+      <BrowserRouter basename={import.meta.env.BASE_URL}>
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
